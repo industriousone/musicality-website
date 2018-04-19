b1f139fdb12f21d01ae0dda63b369447efb134cf
Jason Perkins <starkos@industriousone.com>
2018-04-19 09:22:35 -0400

    Fix all eslint reported issues

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+	<div
+		style={{
+			background: 'rebeccapurple',
+			marginBottom: '1.45rem',
+		}}
+	>
+		<div
+			style={{
+				margin: '0 auto',
+				maxWidth: 960,
+				padding: '1.45rem 1.0875rem',
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						color: 'white',
+						textDecoration: 'none',
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</div>
+	</div>
+);
 
-export default Header
+Header.propTypes = {
+	siteTitle: PropTypes.string.isRequired,
+};
+
+export default Header;
